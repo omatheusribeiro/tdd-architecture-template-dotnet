0086316583330ec6f593854a6d400d36468be30a
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-07 15:11:57 -0300

    Created user repository interfaces

---
+++ src/tdd-architecture-template-dotnet.Domain/Interfaces/Users/IUserAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Users;
+
+namespace tdd_architecture_template_dotnet.Domain.Interfaces.Users
+{
+    public interface IUserAddressRepository
+    {
+        Task<IEnumerable<UserAddress>> GetAll();
+        Task<UserAddress> GetById(int id);
+        Task<UserAddress> Put(UserAddress address);
+        Task<UserAddress> Post(UserAddress address);
+        Task<UserAddress> Delete(UserAddress address);
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Domain/Interfaces/Users/IUserContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Users;
+
+namespace tdd_architecture_template_dotnet.Domain.Interfaces.Users
+{
+    public interface IUserContactRepository
+    {
+        Task<IEnumerable<UserContact>> GetAll();
+        Task<UserContact> GetById(int id);
+        Task<UserContact> GetByEmail(string email);
+        Task<UserContact> Put(UserContact contact);
+        Task<UserContact> Post(UserContact contact);
+        Task<UserContact> Delete(UserContact contact);
+    }
+}
