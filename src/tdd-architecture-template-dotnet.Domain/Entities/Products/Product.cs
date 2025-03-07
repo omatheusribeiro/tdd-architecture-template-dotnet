2fbe4d518402a161a6dfd67da24f283aff74ab67
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-07 15:02:45 -0300

    Created product entities

---
+++ src/tdd-architecture-template-dotnet.Domain/Entities/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Base;
+
+namespace tdd_architecture_template_dotnet.Domain.Entities.Products
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+        public int ProductTypeId { get; set; }
+        public virtual ProductType Type { get; set; }
+        public virtual ICollection<Sale> Sale { get; set; }
+
+        public Product()
+        {
+            Sale = new HashSet<Sale>();
+        }
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Domain/Entities/Products/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Base;
+
+namespace tdd_architecture_template_dotnet.Domain.Entities.Products
+{
+    public class ProductType : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public virtual ICollection<Product> Product { get; set; }
+
+        public ProductType()
+        {
+            Product = new HashSet<Product>();
+        }
+    }
+}
