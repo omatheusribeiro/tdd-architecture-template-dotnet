72b19c9a6f1613132b752314d7f1d2ad9f95a76f
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-07 15:11:31 -0300

    Created product repository interfaces

---
+++ src/tdd-architecture-template-dotnet.Domain/Interfaces/Products/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Products;
+
+namespace tdd_architecture_template_dotnet.Domain.Interfaces.Products
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Put(Product product);
+        Task<Product> Post(Product product);
+        Task<Product> Delete(Product product);
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Domain/Interfaces/Products/IProductTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Products;
+
+namespace tdd_architecture_template_dotnet.Domain.Interfaces.Products
+{
+    public interface IProductTypeRepository
+    {
+        Task<IEnumerable<ProductType>> GetAll();
+        Task<ProductType> GetById(int id);
+        Task<ProductType> Put(ProductType product);
+        Task<ProductType> Post(ProductType product);
+        Task<ProductType> Delete(ProductType product);
+    }
+}
