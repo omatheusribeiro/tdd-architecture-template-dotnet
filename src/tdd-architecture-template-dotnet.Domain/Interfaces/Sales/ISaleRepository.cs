c5e644b869fddb09d7adf1283e458efcfeb8fb8f
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-07 15:11:45 -0300

    Created sales repository interface

---
+++ src/tdd-architecture-template-dotnet.Domain/Interfaces/Sales/ISaleRepository.cs
@@ -0,0 +1,13 @@
+﻿using tdd_architecture_template_dotnet.Domain.Entities.Sales;
+
+namespace tdd_architecture_template_dotnet.Domain.Interfaces.Sales
+{
+    public interface ISaleRepository
+    {
+        Task<IEnumerable<Sale>> GetAll();
+        Task<Sale> GetById(int id);
+        Task<Sale> Put(Sale sale);
+        Task<Sale> Post(Sale sale);
+        Task<Sale> Delete(Sale sale);
+    }
+}
