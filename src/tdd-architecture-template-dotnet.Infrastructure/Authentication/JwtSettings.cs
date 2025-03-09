5b317b33b59d90774fcf9cd4b50d06597ecc5d65
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-08 22:24:53 -0300

    Created application authentication

---
+++ src/tdd-architecture-template-dotnet.Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace tdd_architecture_template_dotnet.Infrastructure.Authentication
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public int ExpirationHours { get; set; } = 2;
+    }
+}
