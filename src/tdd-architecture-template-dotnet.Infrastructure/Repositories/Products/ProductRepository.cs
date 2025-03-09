5c499ca9a58c857099f08f3974e466288062ef7e
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-08 22:27:49 -0300

    Created product repository

---
+++ src/tdd-architecture-template-dotnet.Infrastructure/Repositories/Products/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tdd_architecture_template_dotnet.Domain.Entities.Products;
+using tdd_architecture_template_dotnet.Domain.Interfaces.Products;
+using tdd_architecture_template_dotnet.Infrastructure.Data.Context;
+
+namespace tdd_architecture_template_dotnet.Infrastructure.Repositories.Products
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            return await _context.Products.AsNoTracking().Where(u => u.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> Put(Product product)
+        {
+            product.ChangeDate = DateTime.UtcNow;
+
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Post(Product product)
+        {
+            product.CreationDate = DateTime.UtcNow;
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Delete(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Infrastructure/Repositories/Products/ProductTypeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tdd_architecture_template_dotnet.Domain.Entities.Products;
+using tdd_architecture_template_dotnet.Domain.Interfaces.Products;
+using tdd_architecture_template_dotnet.Infrastructure.Data.Context;
+
+namespace tdd_architecture_template_dotnet.Infrastructure.Repositories.Products
+{
+    public class ProductTypeRepository : IProductTypeRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductTypeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ProductType>> GetAll()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+
+        public async Task<ProductType> GetById(int id)
+        {
+            return await _context.ProductTypes.AsNoTracking().Where(u => u.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<ProductType> Put(ProductType productType)
+        {
+            productType.ChangeDate = DateTime.UtcNow;
+
+            _context.ProductTypes.Update(productType);
+            await _context.SaveChangesAsync();
+
+            return productType;
+        }
+
+        public async Task<ProductType> Post(ProductType productType)
+        {
+            productType.CreationDate = DateTime.UtcNow;
+
+            _context.ProductTypes.Add(productType);
+            await _context.SaveChangesAsync();
+
+            return productType;
+        }
+
+        public async Task<ProductType> Delete(ProductType productType)
+        {
+            _context.ProductTypes.Remove(productType);
+            await _context.SaveChangesAsync();
+
+            return productType;
+        }
+    }
+}
