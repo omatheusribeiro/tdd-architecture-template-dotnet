6f31d09efe8c3a02b80a2af9bfb4dda7131c9877
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-08 22:30:55 -0300

    Infrastructure project initialization file created

--- src/tdd-architecture-template-dotnet.Infrastructure/Startup.cs
+++ src/tdd-architecture-template-dotnet.Infrastructure/Startup.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using tdd_architecture_template_dotnet.Domain.Interfaces.Products;
+using tdd_architecture_template_dotnet.Domain.Interfaces.Sales;
+using tdd_architecture_template_dotnet.Domain.Interfaces.Users;
+using tdd_architecture_template_dotnet.Infrastructure.Data.Context;
+using tdd_architecture_template_dotnet.Infrastructure.Repositories.Products;
+using tdd_architecture_template_dotnet.Infrastructure.Repositories.Sales;
+using tdd_architecture_template_dotnet.Infrastructure.Repositories.Users;
 
 namespace tdd_architecture_template_dotnet.Infrastructure
 {
-    class Startup
+    public static class Startup
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                      .EnableSensitiveDataLogging());
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            #region Users Repositories
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserAddressRepository, UserAddresRepository>();
+            services.AddScoped<IUserContactRepository, UserContactRepository>();
+
+            #endregion
+
+            #region Products Repositories
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductTypeRepository, ProductTypeRepository>();
+
+            #endregion
+
+            #region Sale Repository
+
+            services.AddScoped<ISaleRepository, SaleRepository>();
+
+            #endregion
+
+            return services;
+        }
     }
 }
