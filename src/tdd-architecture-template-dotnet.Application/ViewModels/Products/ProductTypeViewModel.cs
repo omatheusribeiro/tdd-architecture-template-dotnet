ec49b099391ee0b52f5d21595ba8778c5bfece8a
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-10 10:21:10 -0300

    Created product view models

---
+++ src/tdd-architecture-template-dotnet.Application/ViewModels/Products/ProductTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace tdd_architecture_template_dotnet.Application.ViewModels.Products
+{
+    public class ProductTypeViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to enter the product name.")]
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "It is necessary to enter the product name.")]
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Application/ViewModels/Products/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace tdd_architecture_template_dotnet.Application.ViewModels.Products
+{
+    public class ProductViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to enter the product name.")]
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "It is necessary to enter the product name.")]
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "It is necessary to provide the product value.")]
+        [JsonPropertyName("value")]
+        public decimal Value { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to provide the product type id.")]
+        [JsonPropertyName("productTypeId")]
+        public int ProductTypeId { get; set; }
+    }
+}
