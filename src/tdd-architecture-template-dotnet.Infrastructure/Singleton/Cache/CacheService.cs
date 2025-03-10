227bba84a3ccd08722e16641fa58499bac66f835
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-09 22:25:12 -0300

    Cache singleton created

---
+++ src/tdd-architecture-template-dotnet.Infrastructure/Singleton/Cache/CacheService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace tdd_architecture_template_dotnet.Infrastructure.Singleton.Cache
+{
+    public class CacheService
+    {
+        private readonly MemoryCache _cache;
+
+        public CacheService()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        public T? Get<T>(string key)
+        {
+            _cache.TryGetValue(key, out T value);
+            return value;
+        }
+
+        public void Set<T>(string key, T value, TimeSpan expiration)
+        {
+            _cache.Set(key, value, expiration);
+        }
+    }
+
+}
