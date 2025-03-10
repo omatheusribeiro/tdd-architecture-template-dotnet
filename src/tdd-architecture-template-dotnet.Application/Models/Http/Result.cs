afd7ad8ff20df8e5692ef753048fc11211f46a30
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-10 10:26:33 -0300

    http model created

---
+++ src/tdd-architecture-template-dotnet.Application/Models/Http/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace tdd_architecture_template_dotnet.Application.Models.Http
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+        public T? Data { get; set; }
+
+        public static Result<T> Ok(T data, string message = "Success") =>
+            new Result<T> { Success = true, Data = data, Message = message, StatusCode = 200 };
+
+        public static Result<T> Fail(string message, int statusCode = 500) =>
+            new Result<T> { Success = false, Message = message, StatusCode = statusCode };
+    }
+}
