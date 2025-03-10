b3fa52b8f170577de0fa931fab396484ef39505d
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-10 10:21:54 -0300

    Created sales view models

---
+++ src/tdd-architecture-template-dotnet.Application/ViewModels/Sales/SaleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace tdd_architecture_template_dotnet.Application.ViewModels.Sales
+{
+    public class SaleViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to enter the total value.")]
+        [JsonPropertyName("totalValue")]
+        public decimal TotalValue { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to enter the user id.")]
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to provide the product id.")]
+        [JsonPropertyName("productId")]
+        public int ProductId { get; set; }
+    }
+}
