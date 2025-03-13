3fa804e34c0d5b3726b111f08bc1384f7622d7fe
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-12 23:41:23 -0300

    Adjusted startup with dependency injection

--- src/tdd-architecture-template-dotnet.Application/Startup.cs
+++ src/tdd-architecture-template-dotnet.Application/Startup.cs
@@ -1,7 +1,52 @@
-﻿namespace tdd_architecture_template_dotnet.Application
-{
-    public class Startup
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
+using tdd_architecture_template_dotnet.Application.Services.Login.Interfaces;
+using tdd_architecture_template_dotnet.Application.Services.Login;
+using tdd_architecture_template_dotnet.Application.Services.Products.Interfaces;
+using tdd_architecture_template_dotnet.Application.Services.Products;
+using tdd_architecture_template_dotnet.Application.Services.Sales.Interfaces;
+using tdd_architecture_template_dotnet.Application.Services.Sales;
+using tdd_architecture_template_dotnet.Application.Services.Users.Interfaces;
+using tdd_architecture_template_dotnet.Application.Services.Users;
+using tdd_architecture_template_dotnet.Infrastructure.Authentication;
 
+namespace tdd_architecture_template_dotnet.Application
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            #region Authentication Services
+            services.AddScoped<TokenGenerator>();
+            #endregion
+
+            #region Users Services
+
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserAddressService, UserAddresService>();
+            services.AddScoped<IUserContactService, UserContactService>();
+
+            #endregion
+
+            #region Products Services
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductTypeService, ProductTypeService>();
+
+            #endregion
+
+            #region Sales Services
+
+            services.AddScoped<ISaleService, SaleService>();
+
+            #endregion
+
+            #region Login Services
+
+            services.AddScoped<ILoginService, LoginService>();
+
+            #endregion
+
+            return services;
+        }
     }
 }
