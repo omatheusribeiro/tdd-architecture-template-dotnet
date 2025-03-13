7ceeca580594ca4ff4f5d18b1e29a5eb7e5ea3b0
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-12 23:32:34 -0300

    including auto mapper functionality

---
+++ src/tdd-architecture-template-dotnet.Application/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using tdd_architecture_template_dotnet.Application.ViewModels.Products;
+using tdd_architecture_template_dotnet.Application.ViewModels.Sales;
+using tdd_architecture_template_dotnet.Application.ViewModels.Users;
+using tdd_architecture_template_dotnet.Domain.Entities.Products;
+using tdd_architecture_template_dotnet.Domain.Entities.Sales;
+using tdd_architecture_template_dotnet.Domain.Entities.Users;
+
+namespace tdd_architecture_template_dotnet.Application.Mappings
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            #region Users Profile
+
+            CreateMap<User, UserViewModel>();
+            CreateMap<UserAddress, UserAddressViewModel>();
+            CreateMap<UserContact, UserContactViewModel>();
+
+            #endregion
+
+            #region Products Profile
+
+            CreateMap<Product, ProductViewModel>();
+            CreateMap<ProductType, ProductTypeViewModel>();
+
+            #endregion
+
+            #region Sales Profile
+
+            CreateMap<Sale, SaleViewModel>();
+
+            #endregion
+        }
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Application/Mappings/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using tdd_architecture_template_dotnet.Application.ViewModels.Products;
+using tdd_architecture_template_dotnet.Application.ViewModels.Sales;
+using tdd_architecture_template_dotnet.Application.ViewModels.Users;
+using tdd_architecture_template_dotnet.Domain.Entities.Products;
+using tdd_architecture_template_dotnet.Domain.Entities.Sales;
+using tdd_architecture_template_dotnet.Domain.Entities.Users;
+
+namespace tdd_architecture_template_dotnet.Application.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            #region Users Profile
+
+            CreateMap<UserViewModel, User>();
+            CreateMap<UserAddressViewModel, UserAddress>();
+            CreateMap<UserContactViewModel, UserContact>();
+
+            #endregion
+
+            #region Products Profile
+
+            CreateMap<ProductViewModel, Product>();
+            CreateMap<ProductTypeViewModel, ProductType>();
+
+            #endregion
+
+            #region Sales Profile
+
+            CreateMap<SaleViewModel, Sale>();
+
+            #endregion
+        }
+    }
+}
