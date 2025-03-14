d693ef5dfc5d3ac9f1c6afcfd1ac484cedeab623
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-13 23:22:28 -0300

    Added auto mapper configuration

---
+++ src/tdd-architecture-template-dotnet.Api/Config/MappingConfig/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using tdd_architecture_template_dotnet.Application.Mappings;
+
+namespace tdd_architecture_template_dotnet.Config.MappingConfig
+{
+    public static class AutoMapperConfig
+    {
+        public static void AddAutoMapperConfiguration(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddAutoMapper(typeof(DomainToViewModelMappingProfile),
+                    typeof(ViewModelToDomainMappingProfile));
+        }
+    }
+}
