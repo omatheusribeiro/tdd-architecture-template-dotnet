545279d34968e7ea91c94b6d56d16c0f2ca4f740
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-14 20:57:06 -0300

    Created login controller

---
+++ src/tdd-architecture-template-dotnet.Api/Controllers/V1/Login/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tdd_architecture_template_dotnet.Application.Services.Login.Interfaces;
+using tdd_architecture_template_dotnet.Domain.Enums;
+
+namespace tdd_architecture_template_dotnet.Controllers.V1.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpGet("GetLogin")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetLogin(string email)
+        {
+            var response = await _loginService.GetLogin(email);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+    }
+}
