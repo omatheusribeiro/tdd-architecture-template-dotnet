66d6019055c448274595b6582bd35a97098d6a65
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-14 20:58:35 -0300

    Created product controllers

---
+++ src/tdd-architecture-template-dotnet.Api/Controllers/V1/Products/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tdd_architecture_template_dotnet.Application.Services.Products.Interfaces;
+using tdd_architecture_template_dotnet.Application.ViewModels.Products;
+using tdd_architecture_template_dotnet.Domain.Enums;
+
+namespace tdd_architecture_template_dotnet.Controllers.V1.Products
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAllProducts")]
+        [Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _productService.GetAll();
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+
+        [HttpGet("GetProductById/{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var response = await _productService.GetById(id);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+
+        [HttpPut("PutProduct")]
+        [Authorize]
+        public async Task<ActionResult<ProductViewModel>> Put([FromBody] ProductViewModel product)
+        {
+            var response = await _productService.Put(product);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("PostProduct")]
+        [Authorize]
+        public async Task<ActionResult<ProductViewModel>> Post([FromBody] ProductViewModel product)
+        {
+            var response = await _productService.Post(product);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("DeleteProduct")]
+        [Authorize]
+        public async Task<ActionResult<ProductViewModel>> Delete([FromBody] ProductViewModel product)
+        {
+            var response = await _productService.Delete(product);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Api/Controllers/V1/Products/ProductTypeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tdd_architecture_template_dotnet.Application.Services.Products.Interfaces;
+using tdd_architecture_template_dotnet.Application.ViewModels.Products;
+using tdd_architecture_template_dotnet.Domain.Enums;
+
+namespace tdd_architecture_template_dotnet.Controllers.V1.Products
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IProductTypeService _productTypeService;
+
+        public ProductTypeController(IProductTypeService productTypeService)
+        {
+            _productTypeService = productTypeService;
+        }
+
+        [HttpPut("PutProductType")]
+        [Authorize]
+        public async Task<ActionResult<ProductTypeViewModel>> Put([FromBody] ProductTypeViewModel type)
+        {
+            var response = await _productTypeService.Put(type);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("PostProductType")]
+        [Authorize]
+        public async Task<ActionResult<ProductTypeViewModel>> Post([FromBody] ProductTypeViewModel type)
+        {
+            var response = await _productTypeService.Post(type);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
