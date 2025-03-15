a82e58dd7c009e5d735bb87e5b5c58c4c4d5a62c
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-03-14 21:00:32 -0300

    Created user controllers

---
+++ src/tdd-architecture-template-dotnet.Api/Controllers/V1/Users/UserContactController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tdd_architecture_template_dotnet.Application.Services.Users.Interfaces;
+using tdd_architecture_template_dotnet.Application.ViewModels.Users;
+using tdd_architecture_template_dotnet.Domain.Enums;
+
+namespace tdd_architecture_template_dotnet.Controllers.V1.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserContactController : ControllerBase
+    {
+        private readonly IUserContactService _userContactService;
+
+        public UserContactController(IUserContactService userContactService)
+        {
+            _userContactService = userContactService;
+        }
+
+        [HttpPut("PutContact")]
+        [Authorize]
+        public async Task<ActionResult<UserContactViewModel>> Put([FromBody] UserContactViewModel contact)
+        {
+            var response = await _userContactService.Put(contact);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/tdd-architecture-template-dotnet.Api/Controllers/V1/Users/UserController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tdd_architecture_template_dotnet.Application.Services.Users.Interfaces;
+using tdd_architecture_template_dotnet.Application.ViewModels.Users;
+using tdd_architecture_template_dotnet.Domain.Enums;
+
+namespace tdd_architecture_template_dotnet.Controllers.V1.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("GetAllUsers")]
+        [Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _userService.GetAll();
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+
+        [HttpGet("GetUserById/{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var response = await _userService.GetById(id);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+
+        }
+
+        [HttpPut("PutUser")]
+        [Authorize]
+        public async Task<ActionResult<UserViewModel>> Put([FromBody] UserViewModel user)
+        {
+            var response = await _userService.Put(user);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("PostUser")]
+        [Authorize]
+        public async Task<ActionResult<UserViewModel>> Post([FromBody] UserViewModel user)
+        {
+            var response = await _userService.Post(user);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("DeleteUser")]
+        [Authorize]
+        public async Task<ActionResult<UserViewModel>> Delete([FromBody] UserViewModel user)
+        {
+            var response = await _userService.Delete(user);
+
+            if (response.StatusCode == (int)HttpStatus.BadRequest)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
