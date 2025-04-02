9bd1631354690bc535dfccf07de99dde83ce1b2c
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-04-01 22:46:10 -0300

    Includes logger in the login service

--- src/tdd-architecture-template-dotnet.Application/Services/Login/LoginService.cs
+++ src/tdd-architecture-template-dotnet.Application/Services/Login/LoginService.cs
@@ -3,17 +3,20 @@ using tdd_architecture_template_dotnet.Application.Services.Login.Interfaces;
 using tdd_architecture_template_dotnet.Domain.Enums;
 using tdd_architecture_template_dotnet.Domain.Interfaces.Users;
 using tdd_architecture_template_dotnet.Infrastructure.Authentication;
+using tdd_architecture_template_dotnet.Infrastructure.Singletons.Logger.Interfaces;
 
 namespace tdd_architecture_template_dotnet.Application.Services.Login
 {
     public class LoginService : ILoginService
     {
         private IUserContactRepository _userContactRepository;
+        private ILoggerService _loggerService;
         private readonly TokenGenerator _tokenGenerator;
 
-        public LoginService(IUserContactRepository userContactRepository, TokenGenerator tokenGenerator)
+        public LoginService(IUserContactRepository userContactRepository, ILoggerService loggerService, TokenGenerator tokenGenerator)
         {
             _userContactRepository = userContactRepository;
+            _loggerService = loggerService;
             _tokenGenerator = tokenGenerator;
         }
 
@@ -22,12 +25,18 @@ namespace tdd_architecture_template_dotnet.Application.Services.Login
             try
             {
                 if (string.IsNullOrEmpty(email))
+                {
+                    _loggerService.LogInfo("Unable to identify email in the database.");
                     return Result<string>.Fail("Unable to identify email in the database.", (int)HttpStatus.BadRequest);
+                };
 
                 var result = await _userContactRepository.GetByEmail(email);
 
                 if (result is null)
+                {
+                    _loggerService.LogInfo("Unable to identify email in the database.");
                     return Result<string>.Fail("Unable to identify email in the database.", (int)HttpStatus.BadRequest);
+                }
 
                 var token = _tokenGenerator.GenerateToken(result.Email);
 
@@ -36,6 +45,7 @@ namespace tdd_architecture_template_dotnet.Application.Services.Login
             }
             catch (Exception ex)
             {
+                _loggerService.LogError("There was an error generating the token: " + ex.Message);
                 return Result<string>.Fail("There was an error generating the token: " + ex.Message);
             }
 
